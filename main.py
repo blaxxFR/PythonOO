d04c33a34771600702b0713468582bbb4c1837c2
Nathan <amsellem.n@gmail.com>
2022-11-02 19:57:56 +0100

    Commenté

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
+#Avec cet import on va pouvoir créer une classe qui derive de ABC
 from abc import ABC, abstractmethod
 
-class Utilisateur(ABC):
+class Utilisateur(ABC): # Hérite de ABC(Abstract Base Class)
     def __init__(self, nom,prenom,mail,mdp):
         self.nom = nom
         self.prenom = prenom
         self.mail = mail
         self.password = mdp
 
+    @abstractmethod  # Decorateur pour definir une méthode abstraite
     def toString(self):
-        pass
+        pass    # Rien ne se passe à l'interieur de cette methode
 
 
 class Client(Utilisateur):
     def __init__(self, nom, prenom, id, mail, mdp):
-        super().__init__(nom, prenom, mail, mdp)
-        self.id_Client = id
+        super().__init__(nom, prenom, mail, mdp)    # Classe client : possède les attributs de la classe utilisateur
+                                                    # dont elle dérive
+        self.id_Client = id # Possède en plus : un attribut id_Client
 
-    def toString(self):
+    # Si une classe hérite d'une classe ABC, on doit implémenter les méthodes abstraites, autrement, on aura une erreur
+    def toString(self): #évidemment la fonction se doit d'avoir la même définition (nom, arguments) que celle dont elle dérive
         print("Bonjour " + self.prenom + " " + self.nom +" ! Vous êtes un Client")
 
 
 class Salarie (Utilisateur):
     def __init__(self, nom, prenom, id, mdp, salaire):
-        mail = nom + prenom + "@locahub.com"
-        super().__init__(nom, prenom, mail, mdp)
-        self.id_Salarie = id
-        self.salaire = salaire
+        mail = nom + prenom + "@locahub.com" # On créée le mail de l'employé de l'entreprise avec l'adresse de l'entreprise
+        super().__init__(nom, prenom, mail, mdp) # Classe Salarié : possède les attributs de la classe utilisateur
+                                                 # dont elle dérive
+        self.id_Salarie = id # Possède en plus : un attribut id_Salarie
+        self.salaire = salaire #Possède en plus : un attributs salaire
 
+    #De la même manière, toString (méthode abstraite) doit être redéfni pour un Salarié
     def toString(self):
         print("Bonjour " + self.prenom +" "+ self.nom +" ! Vous êtes un Salarié")
+    # Le salarié possèdera une méthode rémunération qui renverra la rémunération mensuelle d'un salarié
+    # Elle fera office de simple getter pour le salarié (pour l'instant) dans la mesure ou l'on retourne seulement
+    # l'attribut salaire
     def remuneration (self) :
         return self.salaire
 
@@ -36,22 +45,28 @@ class Salarie (Utilisateur):
 class Comptable(Salarie):
     def __init__(self, nom, prenom, id, mdp, salaireFixe):
         super().__init__(nom, prenom, id, mdp, salaireFixe)
-
+    # la manière dont est définie la méthode rémunération dans le salarié convient pour le comptable,
+    # on ne redéfinira donc pas cette fonction
 class Commercial(Salarie):
     def __init__(self, nom, prenom, id, mdp, salaireFixe, prime):
-        super().__init__(nom, prenom, id, mdp, salaireFixe)
-        self.prime = prime
+        super().__init__(nom, prenom, id, mdp, salaireFixe) # Classe Commercial : possède les attributs de la
+                                                            # classe Salarié
+        self.prime = prime #Ainsi qu'une prime qui lui sera propre
 
+    # En revanche, un commercial aura des primes qui rentreront en compte dans la rémunération de ce type de poste
+    # Il s'agira donc de redéfinir la fonction rémunération
     def remuneration(self):
         remuneration = self.salaire + self.prime
         return remuneration
 
 
+# Dans cette partie on va se servir du typing afin de montrer le principe de généricité
 from typing import TypeVar, Generic
-
+#T sera le type de variable d'entrées
 T = TypeVar('T')
-
-class Liste_Gestion(Generic[T]):
+#Une classe liste_gestion est définie : elle sera instancié dans la gestion de l'entreprise qui suivra
+class Liste_Gestion(Generic[T]): #On défini Liste_Gestion qui fera dériver des types génériques T
+    # A travers cette classe on pourra donc gérer des listes d'un Type T unique à instancier
     def __init__(self) -> None:
         # Create an empty list with items of type T
         self.items: list[T] = []
@@ -67,11 +82,14 @@ class Liste_Gestion(Generic[T]):
 
     def length(self) -> int :
         return len(self.items)
+
 class Entreprise():
     def __init__(self):
-        # Create an empty list with items of type T
+        # Grace a la classe Liste Gestion on va pouvoir gérer de la manière dont on le souhaite à la fois des clients et
+        # et des employés a l'intereur d'une même classe Liste_Gestion
         self.Clients = Liste_Gestion()
         self.Employes = Liste_Gestion()
+    # la fonction total_Remuneration renverra la rémunération totale émise par l'entreprise à ses salariés
     def totalRemuneration(self):
         total = 0.0;
         x = range(0, self.Employes.length())
@@ -99,9 +117,14 @@ tabemp.append(Salarie3)
 Salarie4 = Commercial("Thedon","Valentin", 4, "3JSPDJX3", 40000, 500)
 tabemp.append(Salarie4)
 
+#Affichage des statuts des personnes créées
+for obj in tabcli:
+    obj.toString()
 for obj in tabemp:
     obj.toString()
 
+
+#On créée l'entreprise qui recevra les Employés et les clients présents dans les 2 listes précédemment affichées
 ETUENTREPRISE = Entreprise()
 
 for i in tabemp:
@@ -109,4 +132,5 @@ for i in tabemp:
 for j in tabcli:
     ETUENTREPRISE.Clients.push(j)
 
+#On affiche la rémuneration totale de l'entreprise vers ses salariés
 print("L'entreprise rémunère au total", ETUENTREPRISE.totalRemuneration()  ,"$ par mois")
